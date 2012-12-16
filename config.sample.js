e86d8b935610b1f2b7d2f6103375cc363cc8fc14
justan <ckr189@gmail.com>
2012-12-17 03:02:20 +0800

    some comments

--- config.sample.js
+++ config.sample.js
@@ -1,10 +1,12 @@
 var config = {
+  //twitter 所需的这些参数可以从 https://dev.twitter.com 新建一个 app 后获取.
   twitter: {
     consumer_key: 'key',
     consumer_secret: 'secret', 
     access_token_key: 'key',
     access_token_secret: 'secet'
   },
+  // weibo 的 access_token 可以从这里授权取得: http://t.cn/zWKjcla
   weibo: {
     access_token: 'weibo'
   }
